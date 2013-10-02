8ec7c6dcb86644e94ac2e4d6e0425b1b2f059b69
David Lecoconnier <etrange02@aol.com>
2013-10-02 12:17:00 +0200

    Transactionable classes and methods added.

---
+++ src/test/transactionablesClasses/ClasseTransactionnable.java
@@ -0,0 +1,39 @@
+package test.transactionablesClasses;
+
+import fr.upmc.aladyn.transactionables.annotations.Transactionable;
+
+@Transactionable
+public class ClasseTransactionnable {
+
+	private int x;
+	private Integer y;
+	
+	public ClasseTransactionnable() {
+		this.x = 0;
+		this.y = 2;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public Integer getY() {
+		return y;
+	}
+
+	public void setY(Integer y) {
+		this.y = y;
+	}
+	
+	public int somme() {
+		return this.x + this.y;
+	}
+	
+	public String toString() {
+		return "CT: x=" + x + "; y=" + y + "; somme=" + somme();
+	}
+}
